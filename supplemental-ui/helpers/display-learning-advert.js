c1aee7c8e04df1de5b0a03066ccba919643ff7f2
David Dieruf <david@dierufventures.com>
2023-06-21 13:33:29 -0400

    Created adverts for learning and Astra

---
+++ supplemental-ui/helpers/display-learning-advert.js
@@ -0,0 +1,8 @@
+'use strict'
+
+module.exports = ({ data: { root } }) => {
+  const pageUrl = root.page.url;
+
+  return (!pageUrl.toLowerCase().includes("/streaming/index.html")
+          && !pageUrl.toLowerCase().includes("streaming-learning"));
+}
